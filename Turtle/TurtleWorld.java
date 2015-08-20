a7ae1d57cd2a1220b09165d3ae56579ef30ea33a
justinthestickman <jchuang@NN123-HA-08.HS.sd203.org>
2015-08-20 15:09:14 -0500

    Explored Turtle

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -9,5 +9,17 @@ public class TurtleWorld
         turtle.setPenColor(Color.RED);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turnRight();
+        turtle.forward(50);
+        turtle.turn(135);
+        turtle.forward(25);
+        turtle.turnLeft();
+        turtle.forward(100);
+        turtle.turnRight();
+        turtle.forward(20);
+        turtle.turnRight();
+        turtle.forward(100);
+        turtle.turnLeft();
+        turtle.forward(25);
     }
 }
