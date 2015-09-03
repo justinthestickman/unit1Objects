c409562b0926cef497acb9f7c5a38e26c97955ba
justinthestickman <jchuang@NN123-HA-08.HS.sd203.org>
2015-09-03 14:59:59 -0500

    Turtle Lab

--- TurtleLab/TurtleWorld.java
+++ TurtleLab/TurtleWorld.java
@@ -5,6 +5,7 @@ public class TurtleWorld
     public static void main(String[] args)
     {
         World turtleWorld = new World();
+        
         Turtle turtle1 = new Turtle(turtleWorld);
         turtle1.setColor(Color.BLUE);
         turtle1.setShellColor(Color.BLUE);
@@ -13,26 +14,24 @@ public class TurtleWorld
         turtle1.penDown();
 
         Turtle turtle2 = new Turtle(turtleWorld);
-        turtle2.setColor(Color.BLUE);
-        turtle2.setShellColor(Color.BLUE);
-        turtle2.setPenColor(Color.BLUE);
-        turtle2.setPenWidth(5);
+        turtle2.setColor(Color.RED);
+        turtle2.setShellColor(Color.RED);
+        turtle2.setPenColor(Color.RED);
+        turtle2.setPenWidth(1);
         turtle2.penDown();
         
-        Turtle turtle3 = new Turtle(turtleWorld);
-        turtle3.setColor(Color.RED);
-        turtle3.setShellColor(Color.RED);
-        turtle3.setPenColor(Color.RED);
-        turtle3.setPenWidth(5);
-        turtle3.penDown();
-        turtle3.turn(180);
-        
         int steps = 0;
-        for (int counter = 1; counter <= 25; counter++)
+        
+        for (int counter = 1; counter <= 200; counter++)
         {
-            steps = steps + 5;
+            steps += 1;
             turtle1.forward(steps);
             turtle1.turn(45);
+        }
+        for (int counter = 1; counter <= 360; counter++)
+        {
+            turtle2.forward(500);
+            turtle2.turn(175);
         }
     }
 }
